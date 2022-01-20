e4930aab6a6c3210c5c6de225b4b80ca4924b0c1
Dong Nguyen <dongnguyen9186@gmail.com>
2022-01-20 18:01:24 +0700

    add coming soon page

---
+++ src/configs.ts
@@ -0,0 +1,6 @@
+export const socialLinks = {
+  discord: 'https://discord.gg/myria',
+  twitter: 'https://twitter.com/myriagames',
+  instagram: 'https://www.instagram.com/myriagames/',
+  medium: 'https://medium.com/@myriagames'
+};
