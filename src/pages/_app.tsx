7719b72f38f3f6919063e27db76551989a54559e
Dong Nguyen <dongnguyen9186@gmail.com>
2022-01-21 17:23:56 +0700

    feat: add seo optimize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,49 @@
-import "video.js/dist/video-js.css";
-import "../styles/globals.css";
-import type { AppProps } from "next/app";
+import 'video.js/dist/video-js.css';
+import '../styles/globals.css';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+import Head from 'next/head';
+import { DefaultSeo } from 'next-seo';
+import type { AppProps } from 'next/app';
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <DefaultSeo
+        title={'Connecting the world through play'}
+        description="Myria is a blockchain gaming ecosystem powered by the Myria blockchain."
+        titleTemplate={'Myria | %s'}
+        openGraph={{
+          type: 'website',
+          locale: 'en',
+          images: [
+            {
+              url: 'https://myria.com/seo/metarush.png',
+              alt: 'Metarush',
+              type: 'image/png'
+            },
+            {
+              url: 'https://myria.com/seo/block_royale.png',
+              alt: 'Block royale',
+              type: 'image/png'
+            },
+            { url: 'https://myria.com/seo/metakart.png', alt: 'Metakart', type: 'image/png' },
+            { url: 'https://myria.com/seo/startstrike.png', alt: 'Startstrike', type: 'image/png' }
+          ],
+          title: 'Myria | Connecting the world through play',
+          description: 'Myria is a blockchain gaming ecosystem powered by the Myria blockchain.',
+          url: 'https://myria.com',
+          site_name: 'Myria'
+        }}
+        twitter={{
+          handle: '@handle',
+          site: '@site',
+          cardType: 'summary_large_image'
+        }}
+      />
+     
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp;
+export default App;
