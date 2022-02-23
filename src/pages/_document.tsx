3511b140212c7b8a2edb9e13f339a95a813c1271
Dong Nguyen <dongnguyen9186@gmail.com>
2022-02-23 17:51:05 +0700

    update ga id

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -22,9 +22,21 @@ class AppDocument extends Document {
           {/* Global Site Tag (gtag.js) - Google Analytics */}
           <script
             async
-            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+            src={`https://www.googletagmanager.com/gtag/js?id=UA-219865262-1`}
           />
           <script
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', "UA-219865262-1", {
+              page_path: window.location.pathname,
+            });
+          `
+            }}
+          />
+          {/* <script
             dangerouslySetInnerHTML={{
               __html: `
             window.dataLayer = window.dataLayer || [];
@@ -35,7 +47,7 @@ class AppDocument extends Document {
             });
           `
             }}
-          />
+          /> */}
         </Head>
         <body>
           <Main />
