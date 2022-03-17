f18581057b84dbd8489fa8d70470856d13d1dacb
Dong Nguyen <dongnguyen9186@gmail.com>
2022-03-17 09:07:24 +0700

    update game card

--- src/components/games/GameList.tsx
+++ src/components/games/GameList.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import React, { useCallback, useRef, useState } from 'react';
 import useClickOutside from 'src/hooks/useClickOutside';
+import Badge from '../Badge';
 import Collapse from '../Collapse';
 import ChevronDownIcon from '../icons/ChevronDownIcon';
 import MinusIcon from '../icons/MinusIcon';
@@ -41,66 +42,76 @@ export const games: {
   genre: Filter[1]['options'][number][];
   publisher: Filter[2]['options'][number];
   id: string;
+  disabled?: true;
 }[] = [
-    {
-      image: '/images/our-games/metarush_op.png',
-      title: 'Metarush',
-      feature: 'Multi Player',
-      genre: ['Survival', 'Racing'],
-      publisher: 'Myria Studios',
-      id: 'metarush',
-    },
-    {
-      image: '/images/our-games/metakart_op.png',
-      title: 'Metakart',
-      feature: 'Multi Player',
-      genre: ['Racing', 'Casual'],
-      publisher: 'Myria Studios',
-      id: 'metakart',
-    },
-    {
-      image: '/images/our-games/block-royale_op.png',
-      title: 'Block Royale ',
-      feature: 'Multi Player',
-      genre: ['Survival', 'Action', 'Shooter'],
-      publisher: 'Myria Studios',
-      id: 'block-royale'
-    },
-    {
-      image: '/images/our-games/starstrike_updated_card_op.png',
-      title: 'Starstrike Legends',
-      feature: 'Multi Player',
-      genre: ['Action', 'Shooter'],
-      publisher: 'Myria Studios',
-      id: 'starstrike',
-    },
-    {
-      image: '/images/our-games/moonville_op.png',
-      title: 'Moonville Farm',
-      feature: 'Single Player',
-      genre: ['Casual', 'Simulation'],
-      publisher: 'Leapblock Studios',
-      id: 'moonville',
-    },
-    {
-      image: '/images/our-games/hot-slice_op.png',
-      title: 'Hot Slice Tycoon',
-      feature: 'Single Player',
-      genre: ['Casual', 'Simulation'],
-      publisher: 'Playware Games',
-      id: 'hotslice'
-    }
-  ];
+  {
+    image: '/images/our-games/metarush_op.png',
+    title: 'Metarush',
+    feature: 'Multi Player',
+    genre: ['Survival', 'Racing'],
+    publisher: 'Myria Studios',
+    id: 'metarush'
+  },
+  {
+    image: '/images/our-games/metakart_op.png',
+    title: 'Metakart',
+    feature: 'Multi Player',
+    genre: ['Racing', 'Casual'],
+    publisher: 'Myria Studios',
+    id: 'metakart'
+  },
+  {
+    image: '/images/our-games/block-royale_op.png',
+    title: 'Block Royale ',
+    feature: 'Multi Player',
+    genre: ['Survival', 'Action', 'Shooter'],
+    publisher: 'Myria Studios',
+    id: 'block-royale'
+  },
+  {
+    image: '/images/our-games/starstrike_updated_card_op.png',
+    title: 'Starstrike Legends',
+    feature: 'Multi Player',
+    genre: ['Action', 'Shooter'],
+    publisher: 'Myria Studios',
+    id: 'starstrike'
+  },
+  {
+    image: '/images/our-games/moonville_op.png',
+    title: 'Moonville Farm',
+    feature: 'Single Player',
+    genre: ['Casual', 'Simulation'],
+    publisher: 'Leapblock Studios',
+    id: 'moonville',
+    disabled: true
+  },
+  {
+    image: '/images/our-games/hot-slice_op.png',
+    title: 'Hot Slice Tycoon',
+    feature: 'Single Player',
+    genre: ['Casual', 'Simulation'],
+    publisher: 'Playware Games',
+    id: 'hotslice',
+    disabled: true
+  }
+];
 
 const GameItem: React.FC<{ item: typeof games[number] }> = ({ item }) => {
   return (
     <Link href={'/game-detail/' + item.id}>
-      <a className="block">
+      <a className={clsx('block', {
+          'pointer-events-none': item.disabled
+        })}>
         <Overlay className="h-[232px] overflow-hidden rounded-[5px] md:h-[344px]">
           <Image src={item.image} alt="" layout="fill" objectFit="cover" />
         </Overlay>
-        <p className="mt-4 text-[14px] font-bold uppercase leading-[1.5] text-brand-light-blue">
-          {item.publisher}
+        <p className="mt-4 flex text-[14px] font-bold uppercase leading-[1.5] text-brand-light-blue">
+          <span>{item.publisher}</span>
+          {item.disabled && <div className="ml-auto">
+            <Badge>
+              <Trans>COMMING SOON</Trans>
+            </Badge>
+          </div>}
         </p>
         <p className="overflow-hidden text-ellipsis whitespace-nowrap text-[16px] font-medium leading-[1.5] md:text-[18px]">
           {item.title}
@@ -212,13 +223,15 @@ const GameList: React.FC = () => {
   const [filter, setFilter] = useState<FilterMap>({
     Features: [],
     Genre: [],
-    'Publisher': []
+    Publisher: []
   });
 
   const filteredGames = games.filter((game) => {
     return (
       (filter['Features'].length == 0 || filter['Features'].includes(game.feature)) &&
-      (filter['Genre'].length == 0 || filter['Genre'].includes(game.genre[0]) || filter['Genre'].includes(game.genre[1])) &&
+      (filter['Genre'].length == 0 ||
+        filter['Genre'].includes(game.genre[0]) ||
+        filter['Genre'].includes(game.genre[1])) &&
       (filter['Publisher'].length == 0 || filter['Publisher'].includes(game.publisher))
     );
   });
