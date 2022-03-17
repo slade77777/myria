8131a553020b4899b36fc995fe292e6bbde58c4f
Dong Nguyen <dongnguyen9186@gmail.com>
2022-03-17 17:33:53 +0700

    update graphs and texts

--- src/pages/blockchain-map.tsx
+++ src/pages/blockchain-map.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BlochainMap: React.FC = () => {
   return (
     <div className="inline-flex min-h-screen bg-black">
-      <img src="/images/ecosystem/diagram_update-2.svg" className=" min-w-[100vw]" alt="" />
+      <img src="/images/ecosystem/Blockchain_Diagram_v2.svg" className="min-w-[100vw]" alt="" />
     </div>
   );
 };
