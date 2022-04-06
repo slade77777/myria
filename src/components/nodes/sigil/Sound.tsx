a37fbf862db09b9c88beb58eab862a67d2befa69
vuongmyria <vuong.dao@myria.com>
2022-04-06 17:53:52 +0700

    chore: add Sound icon

--- src/components/nodes/sigil/Sound.tsx
+++ src/components/nodes/sigil/Sound.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import MuteIcon from 'src/components/icons/MuteIcon';
+import SpeakerIcon from 'src/components/icons/SpeakerIcon';
 import { soundService } from 'src/sound';
-
 
 function Sound() {
   const [mute, setMute] = React.useState(false);
@@ -17,12 +17,7 @@ function Sound() {
   return (
     <footer className="fixed bottom-0 z-10 flex h-[80px] w-full items-center justify-end p-4 px-6">
       <div className="flex cursor-pointer items-center justify-between">
-        {mute && (
-          <div className="mr-3">
-            <MuteIcon />
-          </div>
-        )}
-
+        {<div className="mr-3">{mute ? <MuteIcon /> : <SpeakerIcon />}</div>}
         <p className="body-sm font-medium	" onClick={handleToggleSound}>
           {mute ? 'Unmute sound' : 'Mute sound'}
         </p>
