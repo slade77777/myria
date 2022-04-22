c921bfc4260a5cab87c382e302da9c40fd745585
Dong Nguyen <dongnguyen9186@gmail.com>
2022-04-22 14:03:50 +0700

    add sigil coming soon page

--- src/components/Header/DesktopHeader.tsx
+++ src/components/Header/DesktopHeader.tsx
@@ -64,12 +64,20 @@ const HeaderLinks: React.FC<{ links: NavItem[]; className?: string }> = ({ links
                           onClick={() => {
                             if (item.id === 'community') {
                               if (link.id === 'discord') {
-                                event('Dicord Button Clicked', { button_location: 'Community Links' });
+                                event('Dicord Button Clicked', {
+                                  button_location: 'Community Links'
+                                });
                               }
                               if (link.id === 'twitter') {
-                                event('Twitter Button Clicked', { button_location: 'Community Links' });
+                                event('Twitter Button Clicked', {
+                                  button_location: 'Community Links'
+                                });
                               }
-                              ga.event('Click', { event_category: 'Link', event_label: `${link.text} Link`, value: 'Community Links' })
+                              ga.event('Click', {
+                                event_category: 'Link',
+                                event_label: `${link.text} Link`,
+                                value: 'Community Links'
+                              });
                             }
                           }}
                           target={link.target}
@@ -121,18 +129,25 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
         );
 
       default:
+        // return (
+        //   <a
+        //     onClick={() => {
+        //       event('Dicord Button Clicked', { button_location: 'Top Button' });
+        //       ga.event('Click', { event_category: 'Button', event_label: 'Discord Link', value: 'Top Button' })
+        //     }}
+        //     className="btn-sm btn-secondary"
+        //     href='https://discord.gg/7K49nXJ49R'
+        //     target="_blank"
+        //     rel="noreferrer">
+        //     <Trans>JOIN DISCORD</Trans>
+        //   </a>
+        // );
         return (
-          <a
-            onClick={() => {
-              event('Dicord Button Clicked', { button_location: 'Top Button' });
-              ga.event('Click', { event_category: 'Button', event_label: 'Discord Link', value: 'Top Button' })
-            }}
-            className="btn-sm btn-secondary"
-            href='https://discord.gg/7K49nXJ49R'
-            target="_blank"
-            rel="noreferrer">
-            <Trans>JOIN DISCORD</Trans>
-          </a>
+          <Link href="/sigil">
+            <a className="btn-sm btn-secondary">
+              <Trans>Free Sigil NFT</Trans>
+            </a>
+          </Link>
         );
     }
   }, [action]);
