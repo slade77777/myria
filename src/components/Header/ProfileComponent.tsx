7f896af3993dbb75a6d0d716df2253c868e11029
vuongmyria <vuong.dao@myria.com>
2022-05-04 22:32:28 +0700

    chore: update profile component

--- src/components/Header/ProfileComponent.tsx
+++ src/components/Header/ProfileComponent.tsx
@@ -3,25 +3,13 @@ import { useAuthenticationContext } from 'src/context/authentication';
 import { useWalletContext } from 'src/context/wallet';
 import truncateString from 'src/helper';
 import DashboardIcon from '../icons/DashboardIcon';
-import LinearSettingIcon from '../icons/LinearSettingIcon';
-import SignOutIcon from '../icons/SignOutIcon';
-import WalletIcon from '../icons/WalletIcon';
+import DisconnectIcon from '../icons/DisconnectIcon';
 import Popover from '../Popover';
 
 const ProfileMenus = [
   {
-    text: 'My account',
-    href: '/',
-    icon: <LinearSettingIcon />
-  },
-  {
-    text: 'Wallet',
-    href: '/',
-    icon: <WalletIcon />
-  },
-  {
-    text: 'Node Dashboard',
-    href: '/',
+    text: 'Inventory',
+    href: '/inventory',
     icon: <DashboardIcon />
   }
 ];
@@ -34,18 +22,18 @@ const ProfileComponent = () => {
     return null;
   }
 
-  if (user) {
-    return (
-      <Popover modal>
-        <Popover.Trigger asChild>
-          <div className="flex items-center rounded-3xl bg-[#081824] px-6 py-3 hover:cursor-pointer">
-            <img src={'/images/header-user.png'} alt={address} className="mr-3" />
-            <div>{truncateString(address || '')}</div>
-          </div>
-        </Popover.Trigger>
-        <Popover.Content asChild side="bottom" sideOffset={5}>
-          <div className="min-w-[164px] rounded-xl bg-[#091824] px-4 py-6 text-sm text-white">
-            {ProfileMenus.map((menu) => {
+  return (
+    <Popover modal>
+      <Popover.Trigger asChild>
+        <div className="flex w-[172px] items-center rounded-lg bg-[#081824] px-6 py-3 hover:cursor-pointer">
+          <img src={'/images/header-user.png'} alt={address} className="mr-3" />
+          <div>{truncateString(address || '')}</div>
+        </div>
+      </Popover.Trigger>
+      <Popover.Content asChild side="bottom" sideOffset={5}>
+        <div className="w-[172px] min-w-[164px] rounded-xl bg-[#091824] px-4 py-6 text-sm text-white">
+          {user &&
+            ProfileMenus.map((menu) => {
               return (
                 <Link href={menu.href} key={menu.href}>
                   <a className="mb-6 flex items-center">
@@ -55,29 +43,12 @@ const ProfileComponent = () => {
                 </Link>
               );
             })}
-            <div className="mt-6 flex items-center hover:cursor-pointer">
-              <SignOutIcon />
-              <p className="ml-2">Sign out</p>
+          {address && (
+            <div className="flex items-center hover:cursor-pointer" onClick={disconnect}>
+              <DisconnectIcon />
+              <p className="ml-2">Disconnect</p>
             </div>
-          </div>
-        </Popover.Content>
-      </Popover>
-    );
-  }
-
-  return (
-    <Popover modal>
-      <Popover.Trigger asChild>
-        <div className="flex items-center rounded-3xl bg-[#081824] px-6 py-3 hover:cursor-pointer">
-          <img src={'/images/header-user.png'} alt={address} className="mr-3" />
-          <div>{truncateString(address || '')}</div>
-        </div>
-      </Popover.Trigger>
-      <Popover.Content asChild side="bottom" sideOffset={5}>
-        <div className="flex items-center rounded-xl bg-[#091824] px-6 py-3 text-white">
-          <button onClick={disconnect} className="ml-2">
-            Disconnect
-          </button>
+          )}
         </div>
       </Popover.Content>
     </Popover>
