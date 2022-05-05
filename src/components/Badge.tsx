70a27ee18fbb4cee00ab3e1fce2554dddb36a5cc
Dong Nguyen <dongnguyen9186@gmail.com>
2022-05-05 13:02:22 +0700

    fix styling badge component

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -1,8 +1,8 @@
 const Badge: React.FC<{}> = ({ children }) => {
   return (
-    <span className="rounded-lg bg-brand-dark-blue px-[10px] py-[6px] text-[9px] font-medium leading-[1.3] text-brand-light-blue">
+    <div className="rounded-lg bg-brand-dark-blue px-[10px] py-[6px] text-[9px] leading-[1.3] text-brand-light-blue font-medium">
       {children}
-    </span>
+    </div>
   );
 };
 
