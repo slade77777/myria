d6e01c4326b7b6523b7cf244ee4a6d0b6e8ffc01
vuongmyria <vuong.dao@myria.com>
2022-05-09 14:50:53 +0700

    chore: load sigil profile

--- src/components/nodes/sigil/ChooseAlliance/useChooseAllianceQuery.ts
+++ src/components/nodes/sigil/ChooseAlliance/useChooseAllianceQuery.ts
@@ -1,14 +1,53 @@
-import { useMutation } from 'react-query';
+import { useMutation, useQuery } from 'react-query';
 import http from 'src/client';
+import { AllianceName } from 'src/types/sigil'
+
+interface UsePickSigilQueryParams {
+  onPickSigilSuccess?: () => void;
+  onPickSigilError?: () => void;
+}
+
+type SigilProfileType = {
+  alliance: AllianceName;
+  credits: number;
+  date_registered: Date;
+  user_id: string;
+}
 
 const pickAlliance = (allianceId: string) => {
-  return http.post(`/v1/sigil/users/alliance`, {
+  return http.post(`sigil/users/alliance`, {
     alliance_id: allianceId
   });
 };
 
-export const usePickAllianceMutation = (onSuccess: () => void, onError?: () => void) =>
-  useMutation(pickAlliance, {
-    onSuccess,
-    onError
+export const getSigilProfile = () => http.get('sigil/users/profile').then(res => {
+  const data = res.data?.data;
+  if (data) {
+    const profile: SigilProfileType = {
+      user_id: data.user_id,
+      credits: data.credits,
+      alliance: data.alliance as AllianceName,
+      date_registered: new Date(data.date_registered),
+    };
+    return profile;
+  }
+
+  return null;
+});
+
+export const pickSigilQueryKeys = {
+  pickSigil_profile: 'pickSigil_profile'
+};
+
+export const usePickSigilQuery = (params?: UsePickSigilQueryParams) => {
+  const sigilProfile = useQuery(pickSigilQueryKeys.pickSigil_profile, () => getSigilProfile());
+  const pickSigilMutation = useMutation(pickAlliance, {
+    onSuccess: params?.onPickSigilSuccess,
+    onError: params?.onPickSigilError,
   });
+
+  return {
+    sigilProfile,
+    pickSigilMutation,
+  };
+};
--- src/types/sigil.ts
+++ src/types/sigil.ts
@@ -39,4 +39,5 @@ export type Mission = {
   trackGA4?: () => void;
 };
 
+export type AllianceName = "equinox" | "federation" | "vector_prime" 
 export type RarityType = 'common' | 'rare' | 'ultra rare' | 'epic' | 'celestial'
