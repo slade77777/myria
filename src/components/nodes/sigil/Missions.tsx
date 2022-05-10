b53a256ac16b41b0386445f2e3d0eb2de1895923
Dong Nguyen <dongnguyen9186@gmail.com>
2022-05-10 14:20:24 +0700

    update discord mission url

--- src/components/nodes/sigil/Missions.tsx
+++ src/components/nodes/sigil/Missions.tsx
@@ -62,7 +62,7 @@ const Missions: React.FC = () => {
     },
     JOIN_DISCORD: {
       label: 'Join',
-      link: socialLinks.discord
+      link: process.env.DISCORD_MISSION_URL || socialLinks.discord
     },
     SHARE_TWITTER: {
       label: 'Join',
