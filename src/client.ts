f2fdbfdd7550a4bd03fde72e7d0a6bdc8c08ab42
Dong Nguyen <dongnguyen9186@gmail.com>
2022-05-17 09:16:23 +0700

    integrate klaviyo

--- src/client.ts
+++ src/client.ts
@@ -1,7 +1,15 @@
-import axios from "axios";
+import axios from 'axios';
 
 const apiClient = axios.create({
- baseURL: 'https://api.myria.com',
+  baseURL: 'https://api.myria.com'
+});
+
+export const additionalApiClient = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_ADDITIONAL_API_URL,
+  headers: {
+    accept: 'application/json',
+    'Content-type': 'application/json'
+  }
 });
 
 export default apiClient;
