09087c9d2e5e22be596007809122b7dd0b176da9
Hung Nguyen <hung.nguyen@vogappdevelopers.com>
2022-05-25 14:57:22 +0700

    feat: SIG-30: - Update profile top-right menu component: remove inventory item + refactoring code - Add tooltip to log out item

--- src/components/Header/ProfileComponent.tsx
+++ src/components/Header/ProfileComponent.tsx
@@ -1,11 +1,11 @@
 import { Trans } from '@lingui/macro';
-import Link from 'next/link';
 import { useAuthenticationContext } from 'src/context/authentication';
 import { useWalletContext } from 'src/context/wallet';
 import truncateString from 'src/helper';
 import DashboardIcon from '../icons/DashboardIcon';
 import DisconnectIcon from '../icons/DisconnectIcon';
 import Popover from '../Popover';
+import Tooltip from 'src/components/Tooltip';
 
 const ProfileMenus = [
   {
@@ -18,13 +18,14 @@ const ProfileMenus = [
 const ProfileComponent = () => {
   const { disconnect } = useWalletContext();
   const { user, logout } = useAuthenticationContext();
+  const walletId = user?.wallet_id
 
   const onDisconnect = () => {
     disconnect();
     logout();
   };
 
-  if (!user) {
+  if (!walletId) {
     return null;
   }
 
@@ -32,13 +33,13 @@ const ProfileComponent = () => {
     <Popover modal>
       <Popover.Trigger asChild>
         <div className="flex w-[172px] items-center rounded-lg bg-[#081824] px-6 py-3 hover:cursor-pointer">
-          <img src={'/images/header-user.png'} alt={user?.wallet_id} className="mr-3" />
-          <div>{truncateString(user?.wallet_id || '')}</div>
+          <img src={'/images/header-user.png'} alt={walletId} className="mr-3" />
+          <div>{truncateString(walletId)}</div>
         </div>
       </Popover.Trigger>
       <Popover.Content asChild side="bottom" sideOffset={5}>
         <div className="w-[172px] min-w-[164px] rounded-xl bg-[#091824] px-4 py-6 text-sm text-white">
-          {user &&
+          {/* {user &&
             ProfileMenus.map((menu) => {
               return (
                 <Link href={menu.href} key={menu.href}>
@@ -48,15 +49,26 @@ const ProfileComponent = () => {
                   </a>
                 </Link>
               );
-            })}
-          {user?.wallet_id && (
-            <div className="flex items-center hover:cursor-pointer" onClick={onDisconnect}>
-              <DisconnectIcon />
-              <p className="ml-2">
-                <Trans>Logout</Trans>
-              </p>
-            </div>
-          )}
+            })} */}
+          <div className="flex items-center hover:cursor-pointer" onClick={onDisconnect}>
+            <DisconnectIcon />
+            <Tooltip>
+              <Tooltip.Trigger>
+                <p className="ml-2">
+                  <Trans>Log out</Trans>
+                </p>
+              </Tooltip.Trigger>
+              <Tooltip.Content className="max-w-[256px]">
+                <Tooltip.Arrow />
+                <p>
+                  <Trans>
+                    Logout of Myria, your assets will be safe, this does not disconnect your
+                    wallet from you Myria account
+                  </Trans>
+                </p>
+              </Tooltip.Content>
+            </Tooltip>
+          </div>
         </div>
       </Popover.Content>
     </Popover>
