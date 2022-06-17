a725047722d3952c27e54f2a29ef64664cf05312
hezll <>
2022-06-17 15:58:02 +1000

    no message

--- scripts/deploy_to_s3.sh
+++ scripts/deploy_to_s3.sh
@@ -18,3 +18,5 @@ aws s3 sync --acl public-read  --delete $artifacts_folder s3://$bucket_name
 cloudfront_id=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin==$bucket_name].id" --output text)
 
 aws cloudfront create-invalidation --distribution-id $cloudfront_id --paths '/*'
+
+echo "Deploy to S3 && cloudfront invalidate done."
