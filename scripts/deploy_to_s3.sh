5a0b7f4ee9221b226a5fb5163c4ecd7ae4993a92
hezll <>
2022-06-17 18:01:03 +1000

    no message

--- scripts/deploy_to_s3.sh
+++ scripts/deploy_to_s3.sh
@@ -12,8 +12,7 @@ unset AWS_SECRET_ACCESS_KEY
 # Variables from pipeline
 bucket_name=$1
 artifacts_folder=$2
-cloudfront_id=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='"$bucket_name"'].id" --output text)
-aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='"$bucket_name"'].id" --output text
+cloudfront_id=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$bucket_name'].id" --output text)
 #aws s3 sync --delete $artifacts_folder s3://$bucket_name
 
 
