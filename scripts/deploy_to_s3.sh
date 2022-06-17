cbcace8cc84e257c24b77dfba150bd2cf268e4a9
Joey Don <abbyjia.0811@gmail.com>
2022-06-17 15:04:22 +1000

    feat: Unset default vars in env

--- scripts/deploy_to_s3.sh
+++ scripts/deploy_to_s3.sh
@@ -5,6 +5,10 @@
 
 set -euo pipefail
 
+# Unset AWS env vars from pipeline and use default profile
+unset AWS_ACCESS_KEY_ID
+unset AWS_SECRET_ACCESS_KEY
+
 # Variables from pipeline
 bucket_name=$1
 artifacts_folder=$2
