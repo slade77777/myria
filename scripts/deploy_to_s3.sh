b767f6a5975acefdecca0be01708b9642aca7ecc
hezll <>
2022-06-17 18:36:06 +1000

    test cloudfront id

--- scripts/deploy_to_s3.sh
+++ scripts/deploy_to_s3.sh
@@ -14,7 +14,8 @@ bucket_name=$1
 artifacts_folder=$2
 #aws s3 sync --delete $artifacts_folder s3://$bucket_name
 
+aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$bucket_name'].id" --output text
 
-cloudfront_id=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$bucket_name'].id" --output text) \
-&& aws cloudfront create-invalidation --distribution-id $cloudfront_id --paths '/*'  \
-&& echo "Deploy to S3 && cloudfront invalidate done.$cloudfront_id"
+# cloudfront_id=$(aws cloudfront list-distributions --query "DistributionList.Items[*].{id:Id,origin:Origins.Items[0].Id}[?origin=='$bucket_name'].id" --output text) \
+# && aws cloudfront create-invalidation --distribution-id $cloudfront_id --paths '/*'  \
+# && echo "Deploy to S3 && cloudfront invalidate done.$cloudfront_id"
