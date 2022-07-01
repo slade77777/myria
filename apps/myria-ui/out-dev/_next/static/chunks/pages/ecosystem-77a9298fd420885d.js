58d064e8f5f1486925a992c4feb506235e4390db
Dong Nguyen <dongnguyen9186@gmail.com>
2022-07-01 08:48:10 +0700

    remove build folder

--- apps/myria-ui/out-dev/_next/static/chunks/pages/ecosystem-77a9298fd420885d.js
+++
@@ -1,2 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[1275],{42411:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/ecosystem",function(){return u(62845)}])}},function(n){n.O(0,[6577,7822,794,2845,9774,2888,179],(function(){return _=42411,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
-//# sourceMappingURL=ecosystem-77a9298fd420885d.js.map
