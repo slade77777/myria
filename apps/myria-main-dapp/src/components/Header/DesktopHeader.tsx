a0a71222aa3aae8385cd9d64e43c5cf5cf929ee4
Dong Nguyen <dongnguyen9186@gmail.com>
2022-07-16 18:00:15 +0700

    update connect button

--- apps/myria-main-dapp/src/components/Header/DesktopHeader.tsx
+++ apps/myria-main-dapp/src/components/Header/DesktopHeader.tsx
@@ -1,7 +1,6 @@
 import { Trans } from '@lingui/macro';
 import clsx from 'clsx';
 import Link from 'next/link';
-import { useWalletContext } from 'src/context/wallet';
 import React, { useMemo, useRef } from 'react';
 import { useGA4 } from 'src/lib/ga';
 import { useStickyHeader } from 'src/hooks/useStickyHeader';
@@ -13,18 +12,7 @@ import { Action, NavItem } from './type';
 import { useRouter } from 'next/router';
 import ProfileComponent from './ProfileComponent';
 import LanguageSwitcher from '../LanguageSwitcher';
-import DropdownMenu from '../DropdownMenu';
-import truncateString from 'src/helper';
-import LogoutIcon from '../icons/LogoutIcon';
-import InventoryIcon from 'src/components/icons/InventoryIcon';
-// import { useL2WalletContext } from 'src/context/l2-wallet';
-// import Header from 'src/packages/l2-wallet/src/components/Header';
-import Popover from 'src/packages/l2-wallet/src/components/Popover';
-import ClaimWithdrawPopover from 'src/packages/l2-wallet/src/components/Popover/ClaimWithdrawPopover';
-import L2WalletPopover from 'src/packages/l2-wallet/src/components/Popover/L2WalletPopover';
-import { useSelector } from 'react-redux';
-import { RootState } from 'src/packages/l2-wallet/src/app/store';
-// import CreateMyriaWalletModal from 'l2-wallet/src/components/Modal/CreateMyriaWalletModal';
+import ConnectL2WalletButton from '../ConnectL2WalletButton';
 
 type Props = {
   action: Action;
@@ -42,8 +30,7 @@ const HeaderLinks: React.FC<{ links: NavItem[]; className?: string }> = ({ links
       className={clsx(
         'text-brand-white flex items-center space-x-8 text-[14px] font-semibold uppercase leading-[1.25] tracking-wider',
         className
-      )}
-    >
+      )}>
       {links.map((item, idx) => {
         if (item.inactive) {
           return (
@@ -54,8 +41,7 @@ const HeaderLinks: React.FC<{ links: NavItem[]; className?: string }> = ({ links
                   style={{
                     boxShadow: '0 0 0 0.5px #9AC9E3'
                   }}
-                  className="bg-opacity-4 bg-brand-light-blue/40 absolute -top-[9px] -right-7 rounded-sm p-[3px] pb-[1px] text-[6px] font-extrabold"
-                >
+                  className="bg-opacity-4 bg-brand-light-blue/40 absolute -top-[9px] -right-7 rounded-sm p-[3px] pb-[1px] text-[6px] font-extrabold">
                   <Trans>Soon!</Trans>
                 </div>
               </div>
@@ -89,8 +75,7 @@ const HeaderLinks: React.FC<{ links: NavItem[]; className?: string }> = ({ links
                           target={link.target}
                           className={clsx('hover:text-brand-gold', {
                             'text-brand-gold': link.url === router.pathname
-                          })}
-                        >
+                          })}>
                           {link.text}
                         </a>
                       </Link>
@@ -106,14 +91,12 @@ const HeaderLinks: React.FC<{ links: NavItem[]; className?: string }> = ({ links
               key={idx}
               className={clsx('hover:bg-base/4 rounded-[8px] py-[9px] px-[13px]', {
                 'bg-base/4': item.url === router.pathname
-              })}
-            >
+              })}>
               <Link href={item.url as string}>
                 <a
                   className={clsx('hover:text-blue/6', {
                     'text-blue/6': item.url === router.pathname
-                  })}
-                >
+                  })}>
                   {item.text}
                 </a>
               </Link>
@@ -129,7 +112,6 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
   const { event } = useGA4();
   const headerRef = useRef<HTMLElement>(null);
   useStickyHeader(headerRef, stickyHeader);
-  const { address, onConnect, disconnect } = useWalletContext();
   const { login, user } = useAuthenticationContext();
   const walletModalRef = useRef<any>();
 
@@ -137,28 +119,9 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
     walletModalRef.current.onOpenModal();
   };
 
-  const showClaimPopover = useSelector((state: RootState) => state.ui.showClaimPopover);
-  const popoverRef = useRef<any>();
-
   // const selectTabHandle = (param: number) => {
   //   setActiveTab(param);
   // };
-
-  let abbreviationAddress = '';
-  if (address) {
-    abbreviationAddress = `${address.substring(0, 4)}...${address.substring(
-      address.length - 4,
-      address.length
-    )}`;
-  }
-  // const abbreviationAddress = `${address.substring(0, 4)}...${address.substring(
-  //   account.length - 4,
-  //   account.length,
-  // )}`;
-
-  const closePopover = () => {
-    popoverRef?.current?.closePopover();
-  };
 
   const actionElements = useMemo(() => {
     switch (action) {
@@ -181,8 +144,7 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
               style={{
                 filter: 'drop-shadow(0px 0px 10px #F5B941)'
               }}
-              className="btn-sm btn-secondary"
-            >
+              className="btn-sm btn-secondary">
               <Trans>Free Sigil NFT</Trans>
             </a>
           </Link>
@@ -196,8 +158,7 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
             className="btn-sm btn-secondary"
             href="http://discord.gg/myria"
             target="_blank"
-            rel="noreferrer"
-          >
+            rel="noreferrer">
             <Trans>JOIN DISCORD</Trans>
           </a>
         );
@@ -215,8 +176,7 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
         style={{
           height: navHeight
         }}
-        className="flex w-full grid-cols-[1fr_auto_1fr] items-center gap-4 py-4 lg:px-4 xl:px-[54px]"
-      >
+        className="flex w-full grid-cols-[1fr_auto_1fr] items-center gap-4 py-4 lg:px-4 xl:px-[54px]">
         <div className="items-left mr-12 flex">
           <Link href="/">
             <a className="w-[164px]">
@@ -232,48 +192,7 @@ const DesktopHeader: React.FC<Props> = ({ stickyHeader = true, action }) => {
             <LanguageSwitcher />
           </div>
           <div>
-            {address ? (
-              <div>
-                <Popover
-                  ref={popoverRef}
-                  width="min-w-[406px]"
-                  offsetX={-170}
-                  defaultShow={showClaimPopover}
-                  renderElement={
-                    showClaimPopover ? (
-                      <ClaimWithdrawPopover
-                        abbreviationAddress={abbreviationAddress}
-                        onClosePopover={closePopover}
-                      />
-                    ) : (
-                      <L2WalletPopover
-                        onClosePopover={closePopover}
-                        abbreviationAddress={abbreviationAddress}
-                      />
-                    )
-                  }
-                >
-                  <span className="uppercase">
-                    <button className=" body-14-bold border-base/5 bg-base/3 flex items-center space-x-2 rounded-lg border px-4 py-[9px]"
-                    id="trigger-popover">
-                      <span>{truncateString(address)}</span>
-                      <i className="w-4">
-                        <ChevronDownIcon />
-                      </i>
-                    </button>
-                  </span>
-                </Popover>
-              </div>
-            ) : (
-              <button
-                onClick={() => {
-                  onConnect();
-                }}
-                className="body-14-bold hover:border-primary/7 rounded-lg border border-white py-[9px] px-4 uppercase"
-              >
-                <Trans>Connect wallet</Trans>
-              </button>
-            )}
+            <ConnectL2WalletButton />
           </div>
         </div>
       </nav>
