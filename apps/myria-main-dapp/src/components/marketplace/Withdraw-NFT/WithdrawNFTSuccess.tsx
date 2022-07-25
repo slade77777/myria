aaea8352b5556e583f57b0a6524fdeecea59485f
Khang Nguyen <khang.nguyen@myria.com>
2022-07-25 02:08:56 +0000

    Upgrade SDK version to version 112

--- apps/myria-main-dapp/src/components/marketplace/Withdraw-NFT/WithdrawNFTSuccess.tsx
+++ apps/myria-main-dapp/src/components/marketplace/Withdraw-NFT/WithdrawNFTSuccess.tsx
@@ -11,34 +11,46 @@ const WithdrawNFTSuccess: FC<IProp> = ({}) => {
   const { valueNFT } = useWithDrawNFTContext();
   return (
     <div className="mt-[29px]">
-      <div className='px-[25px]'>
+      <div className="px-[25px]">
         <div className="mx-auto mt-[57px] flex h-[64px] w-[64px] justify-center">
-        <CircleCheckSuccessOutline/>
+          <CircleCheckSuccessOutline />
         </div>
 
         <div className="mt-[24px] text-center text-[24px] text-white">Withdrawal complete</div>
         <div className="text-base/9 text-[14px] text-center mt-4">
-          <span><Trans>Your withdrawal was successful, you should now see your item in your L1 wallet.</Trans></span>
+          <span>
+            <Trans>
+              Your withdrawal was successful, you should now see your item in your L1 wallet.
+            </Trans>
+          </span>
         </div>
       </div>
       <div className="mt-[32px] text-[14px] font-normal rounded-[8px] bg-base/2/[.5] p-4 text-white">
         <div className="flex justify-between">
-          <span className='text-base/9'><Trans>Item</Trans></span>
-          <span className="flex font-medium">
-            {valueNFT.name}
+          <span className="text-base/9">
+            <Trans>Item</Trans>
           </span>
+          <span className="flex font-medium">{valueNFT.name}</span>
         </div>
         <div className="mt-[13px] flex justify-between">
-          <span className='flex items-center text-base/9'><Trans>Transaction ID</Trans></span>
-          <span className='flex items-center text-[#F5B941] font-medium cursor-pointer'><Trans>View</Trans></span>
+          <span className="flex items-center text-base/9">
+            <Trans>Transaction ID</Trans>
+          </span>
+          <span className="flex items-center text-[#F5B941] font-medium cursor-pointer">
+            <Trans>View</Trans>
+          </span>
         </div>
       </div>
       <div className="mt-[138px] flex justify-end">
-        <button onClick={()=>{
-          const triggerWithdraw = document.getElementById('trigger-popover-withdraw');
-          triggerWithdraw?.click();
-        }} className="flex w-full items-center justify-center rounded-[8px] bg-[#F5B941] px-[20px] py-[12px] text-[16px] font-bold text-[#040B10]">
-          <span><Trans>OK</Trans></span>
+        <button
+          onClick={() => {
+            const triggerWithdraw = document.getElementById('trigger-popover-withdraw');
+            triggerWithdraw?.click();
+          }}
+          className="flex w-full items-center justify-center rounded-[8px] bg-[#F5B941] px-[20px] py-[12px] text-[16px] font-bold text-[#040B10]">
+          <span>
+            <Trans>OK</Trans>
+          </span>
         </button>
       </div>
     </div>
