95ec4428ea31f42b468d58e1d45209c792489374
“ldhieu1” <ldhieu1@cmcglobal.vn>
2022-09-08 17:38:00 +0700

    refactor useBalanceList

--- apps/myria-main-dapp/src/packages/l2-wallet/src/common/hooks/useBalanceList.tsx
+++ apps/myria-main-dapp/src/packages/l2-wallet/src/common/hooks/useBalanceList.tsx
@@ -1,24 +1,29 @@
+import _ from 'lodash';
 import { useState, useEffect } from 'react';
+import { useQuery } from 'react-query';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../app/store';
 import { getModuleFactory } from '../../services/myriaCoreSdk';
 
-function useBalanceList(pKey: string, screen: number) {
-  const [balanceList, setBalanceList] = useState<Array<any>>([]);
+function useBalanceList() {
+  const pKey = useSelector(
+    (state: RootState) => state.account.starkPublicKeyFromPrivateKey,
+  );
+  const { data, isLoading, error, isFetched } = useQuery(
+    ['useBalanceList'],
+      async () => {
+        const moduleFactory = await getModuleFactory();
+        if (!moduleFactory || _.isEmpty(pKey)) return;
 
-  useEffect(() => {
-    async function fetchBalanceList() {
-      const moduleFactory = await getModuleFactory();
-      if (!moduleFactory) return;
-
-      const assetModule = moduleFactory.getAssetOnchainManager();
-
-      const assetList = await assetModule.getListAssetsByStarkKey('0x' + pKey);
-
-      setBalanceList(assetList?.data);
+        const assetModule = moduleFactory.getAssetOnchainManager();
+        return assetModule.getListAssetsByStarkKey('0x' + pKey);
+      },
+    {
+      enabled: !!pKey
     }
-    fetchBalanceList();
-  }, [pKey, screen]);
-
-  return { balanceList };
+  );
+  
+  return { data: data?.data || [], isLoading, error, isFetched };
 }
 
 export default useBalanceList;
