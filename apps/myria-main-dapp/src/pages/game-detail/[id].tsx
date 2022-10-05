ca5de50c85422484634939e2e1adca1b772a3134
David Liang <david.liang@myria.com>
2022-10-05 08:57:26 +0000

    fix the issue on the play function

--- apps/myria-main-dapp/src/pages/game-detail/[id].tsx
+++ apps/myria-main-dapp/src/pages/game-detail/[id].tsx
@@ -126,7 +126,7 @@ const GameDetail: React.FC = () => {
                     ))}
                   </div>
                   <div className="order-[-1] mb-6 md:order-1 md:mb-0 md:mt-[48px] ">
-                    {/* <PlayButton gameUrl={gameUrl} /> */}
+                    {!game.isPartner && <PlayButton gameUrl={gameUrl} />}
                     {game.twitter && (
                       <a
                         href={game.twitter}
